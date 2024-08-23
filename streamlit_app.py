eff9263d14fa17dda9bd89fcda65785683b5734d
MennaEraky <mennahesham62@yahoo.com>
2024-08-23 18:47:37 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-st.title("🎈 My new app2")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🎈 Car CO2 Emssionm")
+st.number_input("Feature 1",min_value=1,max_value=10)
+st.number_input("Feature 2",min_value=1,max_value=10)
+st.number_input("Feature 3",min_value=1,max_value=100)
+
+# st.write(
+#     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# )
